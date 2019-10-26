a1c6697101f691f3a61e5b11ff19134b3fefcd1d
Sylvain Boulade <sboulade@gmail.com>
2019-10-26 13:25:17 +0200

    Add api console client for production

--- console-src/src/config.js
+++ console-src/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   appDirectory: "console",
   baseAPIUrl: "https://api.kontist.com",
-  clientId: "TBD",
+  clientId: "fe8055d9-f496-49e9-9551-5b416139630e",
   redirectUri: "https://kontist.dev/console"
 };
