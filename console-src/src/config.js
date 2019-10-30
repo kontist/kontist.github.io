883c03cda1296f1f750a82f5a9e1f60103acd35a
Sylvain Boulade <sboulade@gmail.com>
2019-10-30 10:25:44 +0100

    Update production redirect_uri

--- console-src/src/config.js
+++ console-src/src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   appDirectory: "console",
   baseAPIUrl: "https://api.kontist.com",
   clientId: "fe8055d9-f496-49e9-9551-5b416139630e",
-  redirectUri: "https://kontist.dev/console"
+  redirectUri: "https://kontist.dev/console/"
 };
