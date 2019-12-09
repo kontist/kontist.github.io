4cf4e96963b52f1cc7be655a8fdaf134e2266fbb
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2019-12-09 09:13:02 +0100

    Remove yarn lock files; fix audit problems

---
+++ client-management/precache-manifest.33c537848ad7b0e0d7f69678afeca4b9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226f57719869b3c043aea4953abfdc44",
+    "url": "/client-management/index.html"
+  },
+  {
+    "revision": "3bf8ad298aa9ceb97a81",
+    "url": "/client-management/static/css/main.1e7f5c51.chunk.css"
+  },
+  {
+    "revision": "afe7de4047c73a07eb2e",
+    "url": "/client-management/static/js/2.0c7c8158.chunk.js"
+  },
+  {
+    "revision": "3bf8ad298aa9ceb97a81",
+    "url": "/client-management/static/js/main.597a538c.chunk.js"
+  },
+  {
+    "revision": "78ac733520ebbd8706ab",
+    "url": "/client-management/static/js/runtime-main.b02a528a.js"
+  },
+  {
+    "revision": "2cbfab958b6b5ee39178b3f069eed903",
+    "url": "/client-management/static/media/30D81F_0_0.2cbfab95.eot"
+  },
+  {
+    "revision": "6841604eca61d631de04342ee275fe6c",
+    "url": "/client-management/static/media/30D81F_0_0.6841604e.woff2"
+  },
+  {
+    "revision": "c39f704942e2f4d45516614591cafa74",
+    "url": "/client-management/static/media/30D81F_0_0.c39f7049.ttf"
+  },
+  {
+    "revision": "fef6398eacf2c2f837bdb243f58febaf",
+    "url": "/client-management/static/media/30D81F_0_0.fef6398e.woff"
+  },
+  {
+    "revision": "56dc38ec9f856c5acfd77202f8c957a7",
+    "url": "/client-management/static/media/30D81F_1_0.56dc38ec.woff2"
+  },
+  {
+    "revision": "5c1feab4eb20f6b69e54deace785fc4c",
+    "url": "/client-management/static/media/30D81F_1_0.5c1feab4.woff"
+  },
+  {
+    "revision": "6be05d6470609454b6435226e739ff11",
+    "url": "/client-management/static/media/30D81F_1_0.6be05d64.ttf"
+  },
+  {
+    "revision": "fe062846167a0ecb711069c338b6fc35",
+    "url": "/client-management/static/media/30D81F_1_0.fe062846.eot"
+  },
+  {
+    "revision": "39f188fd98364b61bd5048be2eb8d3b6",
+    "url": "/client-management/static/media/30D81F_2_0.39f188fd.woff"
+  },
+  {
+    "revision": "3d28ffced2e0c4647c75334d9e2dfc1f",
+    "url": "/client-management/static/media/30D81F_2_0.3d28ffce.woff2"
+  },
+  {
+    "revision": "570b634993fc3d4f947d4faaaad36d2d",
+    "url": "/client-management/static/media/30D81F_2_0.570b6349.eot"
+  },
+  {
+    "revision": "f09e438ba29b70803cc21667d8234a93",
+    "url": "/client-management/static/media/30D81F_2_0.f09e438b.ttf"
+  },
+  {
+    "revision": "77f7eff51ca9fb70b5b0e1329dd61f77",
+    "url": "/client-management/static/media/arrow-left.77f7eff5.svg"
+  },
+  {
+    "revision": "33593a9fd6b623cf6e67a10341b8b86d",
+    "url": "/client-management/static/media/checkbox.33593a9f.svg"
+  },
+  {
+    "revision": "66f366acdc323f0910dd3cd37ba3c8ca",
+    "url": "/client-management/static/media/loading-indicator.66f366ac.svg"
+  },
+  {
+    "revision": "3b14b08eccb78116a507690b351b2452",
+    "url": "/client-management/static/media/logo.3b14b08e.svg"
+  }
+]);
